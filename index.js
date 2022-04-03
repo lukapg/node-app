b4452fabb68d87322a4a78075ea2e1ef0ef3c41c
Luka Abazovic <luka.abazovic@outlook.com>
2022-04-03 10:58:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
