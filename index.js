044d0a6dd0ad3cbe5e166b08299382c55491c6b5
Luka Abazovic <luka.abazovic@outlook.com>
2022-04-06 09:59:28 +0200

    test

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ require("./util/passport")(passport);
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
